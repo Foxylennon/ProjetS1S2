081a077b4cde8dd9666344f5f8ebaff2e75ca863
Dante Zanon <dante.zanon@epita.fr>
2025-11-22 13:54:18 +0100

    added script main.py

--- Main.py
+++ Main.py
@@ -8,10 +8,12 @@ game_with,game_height = 1920,1080
 target_ratio = game_with/game_height
 
 
-pygame.init()
-
 screen_x = 1920
 screen_y = 1080
+
+
+pygame.init()
+
 
 # 2. real window ( what the player sees)
 screen = pygame.display.set_mode((screen_x, screen_y))
@@ -19,10 +21,13 @@ screen = pygame.display.set_mode((screen_x, screen_y))
 # 3. creating canvas ( the surface we're going to draw on )
 canvas = pygame.Surface((game_with, game_height))
 
+
+# pygame setup
 clock = pygame.time.Clock()
 running = True
 
 while running:
+
     screen.fill((0,0,0))
 
     #adjusting the canvas scale
@@ -30,15 +35,19 @@ while running:
     scaled_canvas = pygame.transform.scale(canvas, (window_w, window_h))
     screen.blit(scaled_canvas, (0,0))
 
+
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.VIDEORESIZE:
             pass
+
 
 pygame.quit()
